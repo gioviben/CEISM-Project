9923e23e332beb01e1a63f7daaf73371ee85bf86
Grégoire Delignon <gregoire.delignon@student-cs.fr>
2026-04-03 17:32:26 +0200

    Simplification

--- dir_research_idea.py
+++ dir_research_idea.py
@@ -1,22 +1,9 @@
 ### Idée-squelette de la recherche de direction.
 
-from collections import deque
 import numpy as np
 
 # Iterations on k :
-def compute_dir(La,y_queue,s_queue, la_k,grad_la_k):
-    La.pop()
-    La.appendleft(la_k)
-    Grads_la.pop()
-    Grads_la.appendleft(grad_la_k)
-
-    new_y = Grads_la[0] - Grads_la[1]
-    new_s = La[0] - La[1]
-    y_queue.pop()
-    y_queue.appendleft(new_y)
-    s_queue.pop()
-    s_queue.appendleft(new_s)
-
+def compute_dir(new_y,y_queue,s_queue,M):
     q = new_y
     alpha = 0
     for i in range(M):
@@ -38,23 +25,4 @@ def compute_dir(La,y_queue,s_queue, la_k,grad_la_k):
         p = np.dot(y,s)                  # [TP] Scalar product
         beta = np.dot(y,r) / p           # [TP] Scalar product
         r += beta * y
-
-    r *= -1
-    return r
-
-## Initialization : to be put OUTSIDE the loop on k
-M = 5
-D = ... ### problem size : TO BE MODIFIED
-y_la_queue = deque([None for _ in range(M)])
-s_la_queue = deque([None for _ in range(M)])
-y_mu_queue = deque([None for _ in range(M)])
-s_mu_queue = deque([None for _ in range(M)])
-Grads_la = deque([None, None])
-Grads_mu = deque([None, None])
-La = deque([None, None])
-Mu = deque([None, None])
-
-## Implementation INSIDE the loop on k : after STEP 8 (compute gradients) and before STEP 10 (Backtracking Line Search)
-grad_la_k, grad_mu_k = ... , ... ### RETRIEVE FROM STEP 8
-dir_la , dir_mu = compute_dir(La,y_la_queue,la_k,grad_la_k) , compute_dir(Mu,y_mu_queue,mu_k,grad_mu_k)
-# dir_la , dir_mu : INPUTS of STEP 10 (BLS)
+    return -r
