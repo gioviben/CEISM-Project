24149b8464a595800b0577ec8240e6c5369004d0
nicodalmonte <nicolo.dalmonte@hotmail.com>
2026-04-06 18:20:43 +0200

    fixed bugs on lbfgs

--- dir_research_idea.py
+++ dir_research_idea.py
@@ -1,28 +1,28 @@
-### Idée-squelette de la recherche de direction.
-
+### LBFGS ALGORITHM
+import mpi4py
 import numpy as np
 
 # Iterations on k :
 def compute_dir(new_y,y_queue,s_queue,M):
-    q = new_y
-    alpha = 0
+    q = new_y #current gradient 
+    alphas=np.zeros(M)
     for i in range(M):
-        y = y_queue[i]
+        y = y_queue[i] #y_queue[i]= y_{k-i}= g^(k-i)-g^(k-i-1) diff between gradients  
         if y is None:
             break
         s = s_queue[i]
         p = np.dot(y,s)                  # [To Parallelize] Scalar product
-        alpha = np.dot(s,q) / p          # [TP] Scalar product
-        q -= alpha * y
+        alphas[i] = np.dot(s,q) / p          # [TP] Scalar product
+        q -= alphas[i] * y
 
-    r = q
+    r =  q
     beta = 0
-    for i in range(M):
+    for i in reversed(range(M)):
         y = y_queue[i]
         if y is None:
             break
         s = s_queue[i]
         p = np.dot(y,s)                  # [TP] Scalar product
         beta = np.dot(y,r) / p           # [TP] Scalar product
-        r += beta * y
+        r += s*(alphas[i]-beta)
     return -r
